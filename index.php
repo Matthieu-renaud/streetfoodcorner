ba9a795daed7fb8f266c0e73709536f6760b0edb
Matthieu RENAUD <matthieurenaud07@gmail.com>
2021-08-16 11:16:37 +0200

    modif parts

--- index.php
+++ index.php
@@ -1,42 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My Cave</title>
-  <link rel="stylesheet" href="./style.css">
-  <link rel="shortcut icon" href="./assets/img/favicon.png" type="image/x-icon">
-</head>
+include('/parts/header.php');
+session_start();
 
-<body>
-  
-<?php session_start(); ?>
-<header>
-    <nav>
-      <ul class="group_menus">
-        <li class="menus"><a href="./index.php">MyCave</a></li>
-      </ul>
-      <ul class="group_menus">
-        <li class="menus"><a href="./aff_article.php">Vins</a></li>
-        <?php 
-        if (isset($_SESSION['id']))
-        echo '<li class="menus"><a href="./ajout_article.php">Ajout</a></li>';
-      ?>
-      </ul>
-      <ul class="group_menus">
-      <?php 
-        if (isset($_SESSION['id'])) {
-        echo '<li class="menus btn_deco"><a href="./deconnexion.php">Déconnexion</a></li>';
-        } else {
-        echo '<li class="menus btn_co"><a href="./connexion.php">Connexion</a></li>';
-        }
-      ?>
-      </ul>
-    </nav>
-</header>
-
+?>
 
   <main>
 
@@ -89,6 +56,8 @@
 
   </main>
 
-</body>
+<?php
 
-</html>
+include('/parts/footer.php');
+
+?>
--- parts/header.php
+++ parts/header.php
@@ -8,6 +8,27 @@
   <title>Street Food Corner</title>
 </head>
 <body>
+<?php session_start(); ?>
 <header>
-  
+<nav>
+      <ul class="group_menus">
+        <li class="menus"><a href="./index.php">MyCave</a></li>
+      </ul>
+      <ul class="group_menus">
+        <li class="menus"><a href="./aff_article.php">Vins</a></li>
+        <?php 
+        if (isset($_SESSION['id']))
+        echo '<li class="menus"><a href="./ajout_article.php">Ajout</a></li>';
+      ?>
+      </ul>
+      <ul class="group_menus">
+      <?php 
+        if (isset($_SESSION['id'])) {
+        echo '<li class="menus btn_deco"><a href="./deconnexion.php">Déconnexion</a></li>';
+        } else {
+        echo '<li class="menus btn_co"><a href="./connexion.php">Connexion</a></li>';
+        }
+      ?>
+      </ul>
+    </nav>
 </header>
